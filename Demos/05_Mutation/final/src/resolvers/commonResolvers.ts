e336b3db9dd036379e1b194c815174c5b8b120bd
roeb <info@meyer-consulting.net>
2021-09-19 19:33:34 +0200

    Client App added to Module 5

--- Demos/05_Mutation/final/src/resolvers/commonResolvers.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  MutationOperation: {
-    Create: 1,
-    Update: 2,
-    Delete: 3,
-  },
-};
--- Demos/05_Mutation/final/src/resolvers/index.ts
+++
@@ -1,7 +0,0 @@
-import { merge } from 'lodash';
-import attendeeResolvers from "./attendeeResolvers";
-import commonResolvers from './commonResolvers';
-import sessionResolver from './sessionResolver';
-import speakerResolvers from "./speakerResolvers";
-
-export default merge(commonResolvers, speakerResolvers, attendeeResolvers, sessionResolver);
