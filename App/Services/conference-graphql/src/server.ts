61f27b11084abb05ac9f65e60aba78076bac530f
roeb <info@meyer-consulting.net>
2021-09-21 20:18:30 +0200

    Add classic graphql to apps

--- App/Services/conference-graphql/src/server.ts
+++ App/Services/conference-graphql/src/server.ts
@@ -1,32 +1,48 @@
-import 'reflect-metadata';
-import dotenv from "dotenv";
-import { ApolloServer } from "apollo-server-express";
 import 'graphql-import-node';
-import cors from "cors";
+import dotenv from 'dotenv';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
 import compression from 'compression';
-import express from "express";
+import express from 'express';
 import depthLimit from 'graphql-depth-limit';
-import { createServer } from "http";
-import helmet from "helmet";
-import application from './application';
+import { createServer } from 'http';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+import SessionData from './data/sessionData';
+import SpeakerData from './data/speakerData';
+import dbConfig from './dbConfig';
+import AttendeeData from './data/attendeeData';
+import { ApolloServerPluginUsageReporting } from 'apollo-server-core';
+import { PubSub } from 'graphql-subscriptions';
 
-dotenv.config({path: __dirname + "/.env"});
+dotenv.config({ path: __dirname + '/.env' });
 
-const schema = application.createSchemaForApollo();
+export const pubsub = new PubSub();
 
 const app = express();
+const httpServer = createServer(app);
+
 const server = new ApolloServer({
-  schema: schema,
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+  debug: true,
+  dataSources: () => {
+    return {
+      speakerDb: new SpeakerData(dbConfig),
+      sessionApi: new SessionData(),
+      attendeeApi: new AttendeeData(),
+    };
+  },
   validationRules: [depthLimit(7)],
 });
 
+server.installSubscriptionHandlers(httpServer);
+
 app.use(cors());
-app.use(helmet());
 app.use(compression());
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-const httpServer = createServer(app);
-httpServer.listen(
-  { port: 3000 },
-  (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
+httpServer.listen({ port: 3000 }, (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
